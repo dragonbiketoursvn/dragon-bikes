35c51945dcfed7fa19166d258f66b88944ddca59
Patrick Froehlich <dragonbiketoursvn@gmail.com>
2025-06-04 21:08:34 +0700

    testing out cookie consent banner from github.com/Alex-D

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+// $int = random_int(0, 1);
+
+// if ($int) {
+//   require 'landing_page_a.html';
+// } else {
+//   require 'landing_page_b.html';
+// }
+require 'landing_page_b.html';
